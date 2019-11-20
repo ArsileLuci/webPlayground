61cd77e15aed5334bede32569f09994377992fb7
ArsileLuci <arsile410@gmail.com>
2019-11-20 14:49:43 +0300

    reworked with struct color

--- canvas/src/lib.rs
+++ canvas/src/lib.rs
@@ -28,31 +28,40 @@ pub extern "C" fn dealloc(ptr: *mut c_void, cap: usize) {
 pub extern "C" fn fill(pointer: *mut u8, max_width: usize, max_height: usize, time: f64) {
 
     // pixels are stored in RGBA, so each pixel is 4 bytes
-    let byte_size = max_width * max_height * 4;
-    let sl = unsafe { slice::from_raw_parts_mut(pointer, byte_size) };
-
+    let byte_size = max_width * max_height;
+    let mut ptr1 = pointer as *mut Color;
+    let sl = unsafe { slice::from_raw_parts_mut(ptr1, byte_size) };
+    
     for i in 0..byte_size {
 
         // get the position of current pixel
-        let height = i / 4 / max_width;
-        let width  = i / 4 % max_width;
+        let height = i * 4 / 4 / max_width;
+        let width  = i * 4 / 4 % max_width;
 
         if i%4 == 3 {
             // set opacity to 1
-            sl[i] = 255;
+            sl[i].alpha = 255;
         } else if i%4 == 0 {
             // create a red ripple effect from the top left corner
             let len = ((height*height + width*width) as f64).sqrt();
             let nb = time  + len / 4.0;
             let a = 128.0 + nb.cos() * 128.0;
-            sl[i] = a as u8;
+            sl[i].red = a as u8;
         } else if i % 4 == 2 {
             // create a blue ripple effect from the top right corner
             let width = 500 - width;
             let len = ((height*height + width*width) as f64).sqrt();
             let nb = time  + len / 4.0;
             let a = 128.0 + nb.cos() * 128.0;
-            sl[i] = a as u8;
+            sl[i].blue = a as u8;
         }
     }
+}
+
+#[derive(Debug)]
+struct Color {
+    pub red: u8,
+    pub green: u8,
+    pub blue: u8,
+    pub alpha: u8,
 }
